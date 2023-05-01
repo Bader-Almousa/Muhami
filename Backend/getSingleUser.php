158106292868f3a7c18ec8a343294b356aa99284
Bader <Baderalmousa@Mail.com>
2023-05-01 12:09:30 +0300

    php api update

--- Backend/getSingleUser.php
+++ Backend/getSingleUser.php
@@ -3,11 +3,12 @@
 include "config.php";
 
 $data = array();
-$Id = $_GET['Id'];
-$q = mysqli_query($con, "SELECT * FROM `user` WHERE `Id` = $Id LIMIT 1");
+$id = $_GET['id'];
+$q = mysqli_query($conn, "SELECT * FROM `user` WHERE `id` = $id LIMIT 1");
 
 while($row = mysqli_fetch_object($q)){
     $data[] = $row;
 }
 echo json_encode($data);
-echo mysqli_error($con);
+echo mysqli_error($conn);
+?>
