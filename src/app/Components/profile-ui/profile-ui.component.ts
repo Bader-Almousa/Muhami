5129ad8b8ba4661e3c55165b15850de911a1ca8d
Amin <ameen44446@gmail.com>
2023-05-28 13:28:57 +0300

    hi

--- src/app/Components/profile-ui/profile-ui.component.ts
+++ src/app/Components/profile-ui/profile-ui.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileUiComponent  implements OnInit {
 
+  
+  firstName: any;
+  lastName: any;
+  phoneNumber: any;
+  email: any;
+  password: any;
+  license: any;
+
+  isChecked: any;
+  photo = '.\assets\img\FinallogoGold.png';
   constructor() { }
 
   ngOnInit() {}
