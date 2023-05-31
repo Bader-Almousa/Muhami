3d78da9d687ee0ce6e84b52eccacfff963da9c21
Bader <Baderalmousa@Mail.com>
2023-05-31 14:46:23 +0300

    hjujuj

--- src/app/Components/profile-ui/profile-ui.component.ts
+++ src/app/Components/profile-ui/profile-ui.component.ts
@@ -20,7 +20,7 @@ export class ProfileUiComponent  implements OnInit {
     imageUrl: 'https://docs-demo.ionic.io/assets/madison.jpg'
   };
 
-  isChecked: any;
+
   photo = '.\assets\img\FinallogoGold.png';
   
 
@@ -28,7 +28,38 @@ export class ProfileUiComponent  implements OnInit {
     public alertController: AlertController, 
     public navCtrl: NavController) {}
 
-    async showLoading() {
+    ngOnInit() {
+      const id = localStorage.getItem('id');
+      const firstName = localStorage.getItem('firstName');
+      const lastName = localStorage.getItem('lastName');
+      const phoneNumber = localStorage.getItem('phoneNumber');
+      const email = localStorage.getItem('email');
+  
+      this.user.id = id ? id : '';
+      this.user.firstName = firstName ? firstName : '';
+      this.user.lastName = lastName ? lastName : '';
+      this.user.phoneNumber = phoneNumber ? phoneNumber : '';
+      this.user.email = email ? email : '';
+    }
+  
+    async presentAlert(mess: any) {
+      setTimeout(async () => {
+        const alert = await this.alertController.create({
+          message: mess,
+          buttons: ['حسنًا'],
+        });
+        await alert.present();
+      }, 1650); // تأخير ظهور الـ Alert لمدة 5 ثواني
+    }
+
+    async save() {
+
+        const loading = await this.loadingCtrl.create({
+          message: 'جاري الحفظ',
+          duration: 1500,
+          spinner: 'circles'
+        });
+        loading.present();
 
     const formData = new FormData();
       formData.append('id', this.user.id);
@@ -42,16 +73,13 @@ export class ProfileUiComponent  implements OnInit {
         console.log(response);
       },
       (error) => {
-
         // إنشاء وعرض رسالة تنبيه باستخدام اسم المستخدم
         const alertMessage = 'شكرا ' + this.user.firstName + '، تم تحديث بياناتك بنجاح';
         this.presentAlert(alertMessage);
         console.log(error);
       }
     );
-
-<<<<<<< HEAD
-
+    }
 
   updateProfilePicture(event: Event) {
     const fileInput = event.target as HTMLInputElement;
@@ -66,43 +94,4 @@ export class ProfileUiComponent  implements OnInit {
     }
   }
 
- 
-
-
-
-  async showLoading() {
-=======
->>>>>>> a15ea5622d110e32f45947dcb12223302e58a4cc
-    const loading = await this.loadingCtrl.create({
-      message: 'جاري الحفظ',
-      duration: 1500,
-      spinner: 'circles'
-    });
-
-    loading.present();
-  }
-
-  ngOnInit() {
-    const id = localStorage.getItem('id');
-    const firstName = localStorage.getItem('firstName');
-    const lastName = localStorage.getItem('lastName');
-    const phoneNumber = localStorage.getItem('phoneNumber');
-    const email = localStorage.getItem('email');
-
-    this.user.id = id ? id : '';
-    this.user.firstName = firstName ? firstName : '';
-    this.user.lastName = lastName ? lastName : '';
-    this.user.phoneNumber = phoneNumber ? phoneNumber : '';
-    this.user.email = email ? email : '';
-  }
-
-  async presentAlert(mess: any) {
-    setTimeout(async () => {
-      const alert = await this.alertController.create({
-        message: mess,
-        buttons: ['حسنًا'],
-      });
-      await alert.present();
-    }, 1650); // تأخير ظهور الـ Alert لمدة 5 ثواني
-  }
 }
