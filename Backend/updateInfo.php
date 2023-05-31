a15ea5622d110e32f45947dcb12223302e58a4cc
Bader <Baderalmousa@Mail.com>
2023-05-31 14:20:33 +0300

    1233333

---
+++ Backend/updateInfo.php
@@ -0,0 +1,22 @@
+<?php
+include "database.php";
+
+$db = new Database();
+$conn = $db->connect();
+
+// استلام البيانات المرسلة من الطلب
+$id = $_POST['id'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$phoneNumber = $_POST['phoneNumber'];
+$email = $_POST['email'];
+
+// تحديث بيانات المستخدم في جدول MySQL
+$sql= "UPDATE users SET firstName='$firstName', lastName='$lastName', phoneNumber='$phoneNumber', email='$email' WHERE id='$id'";
+
+if ($conn->query($sql) === TRUE) {
+  echo "تم تحديث بيانات المستخدم بنجاح";
+} else {
+  echo "خطأ في تحديث بيانات المستخدم: " . $conn->error;
+}
+?>
--- src/app/pages/edit-profile/edit-profile.page.ts
+++ src/app/pages/edit-profile/edit-profile.page.ts
@@ -9,91 +9,12 @@ import { AlertController, NavController } from '@ionic/angular';
 })
 export class EditProfilePage implements OnInit {
 
-  info = {
-    id: '',
-    firstName: '',
-    lastName: '',
-    phoneNumber: '',
-    email: '',
-    password: '',
-    license: ''
-  };
-  isLawyer = false;
-
   constructor(
-    private http: HttpClient,
-    public alertController: AlertController,
-    public navCtrl: NavController
+
   ) {}
 
   ngOnInit() {
-    // استدعاء الـ API لاسترجاع معلومات المستخدم
-    const id = localStorage.getItem('userId');
-    const isLawyer = localStorage.getItem('isLawyer');
-    const formData = new FormData();
-    if (id) {
-      formData.append('id', id);
-    }
-    if (isLawyer) {
-      formData.append('isLawyer', isLawyer);
-    }
-    this.isLawyer = isLawyer === 'true';
-
-    this.http
-      .post('http://localhost/Projects/Muhami/Backend/getUser.php', formData)
-      .subscribe(
-        (response: any) => {
-          console.log(response);
-          this.info.id = response.data.id;
-          this.info.firstName = response.data.firstName;
-          this.info.lastName = response.data.lastName;
-          this.info.phoneNumber = response.data.phoneNumber;
-          this.info.email = response.data.email;
-          this.info.password = response.data.password;
-          if (this.isLawyer) {
-            this.info.license = response.data.license;
-          }
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
+    
   }
 
-  updateProfile() {
-    // إرسال البيانات المحدثة إلى الـ API لتحديثها في قاعدة البيانات
-    const formData = new FormData();
-    formData.append('id', this.info.id);
-    formData.append('firstName', this.info.firstName);
-    formData.append('lastName', this.info.lastName);
-    formData.append('phoneNumber', this.info.phoneNumber);
-    formData.append('email', this.info.email);
-    formData.append('password', this.info.password);
-    if (this.isLawyer) {
-      formData.append('license', this.info.license);
-    }
-
-    this.http
-      .post('http://localhost/Projects/Muhami/Backend/updateUser.php', formData)
-      .subscribe(
-        (response) => {
-          console.log(response);
-          const alertMessage = 'تم تحديث المعلومات الشخصية بنجاح';
-          this.presentAlert(alertMessage);
-          this.navCtrl.navigateForward('/tabs/home');
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  }
-
-  async presentAlert(mess: any) {
-    const alert = await this.alertController.create({
-      message: mess,
-      buttons: ['حسنًا'],
-    });
-
-    await alert.present();
-  }
 }
